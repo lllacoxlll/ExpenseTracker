545a4a789e36eb5c770abde2209e19f4d6ffe4f6
lllacoxlll <lllacoxlll@gmail.com>
2023-05-26 13:35:04 -0500

    Initial commit

---
+++ ExpenseTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Expense_Tracker.Models
+{
+	public class ApplicationDbContext:DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options):base(options)
+		{}
+
+		public DbSet<Transaction> Transactions { get; set; }
+		public DbSet<Category> Categories { get; set; }
+
+    }
+}
+
